ca57b3aea4aa5af891c945560603cf10c748e432
abooabdirahman <abuabdirahman6@gmail.com>
2020-04-13 22:46:24 +0100

    getting along

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,12 @@
 const covid19ImpactEstimator = (data) => {
     const input = data;
-    impact.currentlyInfected = data.currentlyInfected * 10;
-    severeImpact.currentlyInfected = data.reportedCases * 50;
-
-    infectionsByRequestedTime =
-    data.impact * 1024
-    data.severeImpact * 1024
-    data.currentlyInfected * 1024;
-
-    return {
+  impact.currentlyInfected = data.currentlyInfected * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  infectionsByRequestedTime =
+  data.impact * 1024
+  data.severeImpact * 1024
+  data.currentlyInfected * 1024;
+  return {
         data: input,
         impact: {},
         severeImpact: {},
