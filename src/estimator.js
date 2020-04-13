f182eb209856158a0fdff9a21998b5958966acee
abooabdirahman <abuabdirahman6@gmail.com>
2020-04-13 18:40:11 +0100

    On the estimator

--- src/estimator.js
+++ src/estimator.js
@@ -3,8 +3,16 @@ const covid19ImpactEstimator = (data) => {
     return {
         data: input,
         impact: {},
-        severeImpact: {}
+        severeImpact: {},
+        reportedCases: {},
+        currentlyInfected: {}
     };
+    impact.currentlyInfected = this.currentlyInfected * 10;
+    severeImpact.currentlyInfected = this.reportedCases * 50;
+    const infectionsByRequestedTime = 
+    this.impact * 1024
+    this.severeImpact * 1024
+    this.currentlyInfected * 1024;
 };
 
 export default covid19ImpactEstimator;
